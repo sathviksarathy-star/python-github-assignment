d0984ff2b058736c37e1aba71d32ec47a0316408
sathviksarathy-star <sathviksarathy@gmail.com>
2025-11-24 16:58:08 -0500

    Refactor docstring and add user input prompt
    Asks the user the question about how much money they saved

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ estimates total monthly savings, and shows progress toward a monthly goal.
 # --- Task 1: Welcome message ---
 print("Welcome to the Monthly Savings Estimator!")
 
+# --- Task 2: Ask for user input ---
+daily_input = input("How much money did you save today? $")
 
