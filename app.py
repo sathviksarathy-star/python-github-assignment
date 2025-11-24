abef5375c45623b800c0299db765959f632cfad4
sathviksarathy-star <sathviksarathy@gmail.com>
2025-11-24 16:58:41 -0500

    Reformat comments and add monthly savings goal
    Sets a monthly saving goal

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ print("Welcome to the Monthly Savings Estimator!")
 # --- Task 2: Ask for user input ---
 daily_input = input("How much money did you save today? $")
 
+# --- Task 3 + Task 5: Convert input, perform calculation, handle errors ---
+MONTHLY_GOAL = 300  # Set a monthly savings goal
